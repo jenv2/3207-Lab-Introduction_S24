e7cd841ef224aeb115a6ebc4e5bc44a24e475c0b
jenv2 <156946964+jenv2@users.noreply.github.com>
2024-01-17 13:26:12 -0500

    Update random.c

--- random.c
+++ random.c
@@ -1,1 +1,2 @@
-# function randchar() returns a random character from A-Z
+// function randchar() returns a random character from A-Z
+// this function is then used in print_random.c to generate a 7 letter word
